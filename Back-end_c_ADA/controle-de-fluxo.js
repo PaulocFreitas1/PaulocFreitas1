535423097ac6b068a8308472e2f571c9054d7bbc
PaulocFreitas1 <paulocesar012.pc@gmail.com>
2024-12-13 18:04:16 -0300

    adicionado arquivo de controle

---
+++ Back-end_c_ADA/controle-de-fluxo.js
@@ -0,0 +1,24 @@
+const numero1= 100
+const numero2= 20
+const numeroCorreto= 30
+const stringDeNumero=30
+
+if (numero1 > 1) {
+console.log('Este numero é maior que 1');
+    
+}
+if (numero1>200) {
+    console.log('Este numero é maior que 200')
+ }else{
+    console.log('Este numero é menor que 200')
+ }
+ 
+if (numero2<=numero1) {
+    console.log(`${numero2} é menor ou igual a ${numero1}`) 
+}
+if (numeroCorreto == stringDeNumero) {
+    console.log('Os numero sao iguais em valor apenas (podem ter alterações implicitas ')
+}
+    if (numeroCorreto === stringDeNumero) {
+        console.log('Os numero sao iguais em valor e tipo')
+    }
