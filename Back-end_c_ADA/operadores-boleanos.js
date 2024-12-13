13854485631ad7c0a05ce181f6dc63c445659c0d
PaulocFreitas1 <paulocesar012.pc@gmail.com>
2024-12-13 18:22:23 -0300

    adicionado arquivo de operadores booleanos

---
+++ Back-end_c_ADA/operadores-boleanos.js
@@ -0,0 +1,22 @@
+if (true) {
+    console.log('sempre aparecerei')
+}
+if (false) {
+    console.log('nao aparecerei')
+}
+
+//ou : ||
+//e : &&
+//expressoes sempre retornam verdadeiras se houver 1 verdadeira.
+
+//console.log('true || true')
+//console.log('true || false')
+//console.log('false || true')
+//console.log('false || false')
+
+//console.log('1< -20 || 100<5 || 30>10')
+
+console.log(true && true)
+console.log(true && false)
+console.log(false && true)
+console.log(false && false)
