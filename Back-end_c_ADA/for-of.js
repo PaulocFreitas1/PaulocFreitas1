6f8a51dfbecfd4d72a8f033e77653549bf9e0317
PaulocFreitas1 <paulocesar012.pc@gmail.com>
2024-12-15 12:11:23 -0300

    adicionado 2 itens, for-os e objetos

---
+++ Back-end_c_ADA/for-of.js
@@ -0,0 +1,11 @@
+const listaDeNumeros=[25, 88, 32, 40, 101, 97]
+const usuarios =['Ana', 'Paulo', 'Murilo', 'Eduarda']
+
+console.log("Print via for \n")
+for (const usuario of usuarios){
+    console.log(usuario)
+}
+// o numero ou usuario tanto faz o nome, so usei o elemento com nome parecido para facilitar
+for (const numero of listaDeNumeros){
+    console.log(numero)
+}
---
+++ Back-end_c_ADA/objetos.js
@@ -0,0 +1,13 @@
+const animal = {
+    especie: 'jacare',
+    habitatNatural: 'florestas e pantanos',
+    ehTerreste: true
+}
+console.log(animal.ehTerreste)
+console.log(animal.habitatNatural)
+console.log(animal.especie)
+
+animal.populacacao = 100000000
+console.log(animal.populacacao)
+
+
