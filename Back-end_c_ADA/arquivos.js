e6117126afe4aec63f7d224c944337faa686c663
PaulocFreitas1 <paulocesar012.pc@gmail.com>
2024-12-17 19:13:16 -0300

    adicionado 2 itens, 1 criado por manipulação e puxando o texto com path

--- Back-end_c_ADA/arquivos.js
+++ Back-end_c_ADA/arquivos.js
@@ -2,4 +2,26 @@ const path= require('node:path') // esse node é para garantir q busq so no node
 const fs=require('node:fs')
 
 const filePath = path.join(process.cwd(), 'texto.txt')
-console.log('filePath')
+const fileOutPath = path.join(process.cwd(), 'texto-com-linhas.txt')
+
+
+fs.readFile(filePath, {}, (erro, dados)=>{
+if(erro) {
+    console.log(`Erro na leitura do arquivo no caminho ${filePath}`)
+    return
+}
+const texto =dados.toString()
+const linhas = texto.split('\n')
+
+const linhasAjustadas= linhas.map((linha, index, ArrayDeLinhas)=> 
+`${index + 1} - ${linha}`
+)
+fs.writeFile(fileOutPath, linhasAjustadas.join('\n'), {}, (erro)=> {//sobrescrever arquivo
+    if(erro) {
+         console.log(`Erro na leitura do arquivo no caminho ${fileOutPath}`)
+     }
+    })
+})
+
+//console.log(dados.toString()) para converter para string
+//console.log('filePath')
